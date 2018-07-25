056b55d687c0f6797b73006c9924cbe606feeb7e
rnjhstjdgh <gshgsh0831@daum.net>
2018-07-25 22:48:31 +0900

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'm1g3d16=48v%ve%ayc9l6fhwd%^rq2i4ji@n$!&n4r^mngtwiz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
